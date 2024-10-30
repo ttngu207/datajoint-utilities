31d50807933dc897574d26ee6175e2cd646e5d64
Thinh Nguyen <thinh@datajoint.com>
2024-10-30 16:53:00 -0500

    feat: get to prefect 3.0

--- datajoint_utilities/dj_prefect/flow.py
+++ datajoint_utilities/dj_prefect/flow.py
@@ -10,14 +10,13 @@ import re
 from datetime import datetime, timedelta
 
 from prefect import task, flow
-from prefect.deployments import Deployment, run_deployment
-from prefect.orion.schemas.schedules import IntervalSchedule
-
+from prefect.deployments import run_deployment
 from prefect.filesystems import LocalFileSystem
+from prefect.logging import get_run_logger
 
-logger = dj.logger
 
 _populate_settings = {
+    "display_progress": True,
     "reserve_jobs": True,
     "suppress_errors": True,
 }
@@ -31,7 +30,6 @@ class DataJointFlow:
     def __init__(
         self,
         flow_name,
-        storage=None,
         trigger_poll_interval=600,
     ):
         self.flow_name = flow_name
@@ -42,9 +40,7 @@ class DataJointFlow:
         self.task_count = -1
         self._terminal_process = None
 
-        self.storage = storage or LocalFileSystem()
-
-        self.flows, self.deployments = {}, {}
+        self.flows = {}
 
         self._main_flow_name = self.flow_name
         self._trigger_flow_name = self._main_flow_name + "_trigger"
@@ -52,24 +48,31 @@ class DataJointFlow:
         self._trigger_flow_deploy_name = self._trigger_flow_name + "-deploy"
 
     def __call__(self, process):
+        """
+        Decorator to add a processing step as one Prefect "Task" to the Prefect "Flow"
+        """
+
         self.task_count += 1
         self.processes[self.task_count] = process
         if isinstance(process, dj.user_tables.TableMeta):
 
             @task(name=process.__name__)
             def flow_task(restrictions):
-                job_errors = process.populate(*restrictions, **_populate_settings)
-                if len(job_errors):
-                    error_msg = (
-                        f"Populate Task failed - {len(job_errors)} job errors.\n"
-                    )
-                    error_msg += "\n".join([j for _, j in job_errors])
+                logger = get_run_logger()
+
+                status = process.populate(*restrictions, **_populate_settings)
+                success_count = status["success_count"]
+                error_list = status["error_list"]
+                logger.info(f"Populate status - {success_count} jobs succeeded.")
+                if len(error_list):
+                    logger.error(f"Populate status - {len(error_list)} job errors.")
                     # delete error jobs from job-table
                     vmod = dj.create_virtual_module(process.database, process.database)
                     (
                         vmod.schema.jobs
-                        & [{"key_hash": dj.hash.key_hash(k)} for k, _ in job_errors]
+                        & [{"key_hash": dj.hash.key_hash(k)} for k, _ in error_list]
                     ).delete()
+                    error_msg = "\n".join([j for _, j in error_list])
                     raise dj.errors.DataJointError(error_msg)
 
             self.tasks[self.task_count] = flow_task
@@ -88,6 +91,10 @@ class DataJointFlow:
 
     @property
     def main_flow(self):
+        """
+        Assemble the primary (main) Prefect "Flow" from all the tasks
+        """
+
         if "main" not in self.flows:
 
             @flow(name=self._main_flow_name)
@@ -108,6 +115,7 @@ class DataJointFlow:
     @property
     def trigger_flow(self):
         """
+        Creates a secondary Flow (named "trigger") to schedule new flow-runs of the main flow
         This flow accomplishes two goals
         1. schedule new flow runs based on the `key_source` of the parent table
         2. cancel staled scheduled runs
@@ -120,6 +128,8 @@ class DataJointFlow:
 
             @task(name="create_main_runs")
             def create_main_runs():
+                logger = get_run_logger()
+
                 keys_todo = (
                     self.processes[0].key_source
                     - self.processes[self._terminal_process]
@@ -138,57 +148,58 @@ class DataJointFlow:
                         idempotency_key=dj.hash.key_hash(key),
                         timeout=0,
                     )
-
-            @task(name="create_trigger_runs")
-            def create_trigger_run():
-                # Queue up one next flow run for trigger_flow
-                run_deployment(
-                    name=f"{self._trigger_flow_name}/{self._trigger_flow_deploy_name}",
-                    timeout=0,
-                    scheduled_time=datetime.utcnow()
-                    + timedelta(seconds=self.trigger_interval),
-                )
+            #
+            # @task(name="create_trigger_runs")
+            # def create_trigger_run():
+            #     # Queue up one next flow run for trigger_flow
+            #     run_deployment(
+            #         name=f"{self._trigger_flow_name}/{self._trigger_flow_deploy_name}",
+            #         timeout=0,
+            #         scheduled_time=datetime.utcnow()
+            #         + timedelta(seconds=self.trigger_interval),
+            #     )
 
             @flow(name=self._trigger_flow_name)
             def _trigger():
                 create_main_runs()
-                create_trigger_run()
+                # create_trigger_run()
 
             self.flows["trigger"] = _trigger
 
         return self.flows["trigger"]
 
-    def deploy(self):
-        if "main" in self.deployments and "trigger" in self.deployments:
-            return
+    def deploy(self, source=None, image=None, build=False, push=False, work_queue_name=None, work_pool_name=None):
+        source = source or LocalFileSystem()
 
         calling_frame = inspect.getouterframes(inspect.currentframe())[1]
-
         datajoint_flow_name = re.search(
-            r"\s?(\S+)\.deploy\(\)", calling_frame.code_context[0]
+            r"\s?(\S+)\.deploy\(.*", calling_frame.code_context[0]
         ).groups()[0]
 
         calling_package = calling_frame.frame.f_globals["__package__"]
         calling_file = pathlib.Path(calling_frame.frame.f_globals["__file__"]).name
 
-        main_deployment = Deployment.build_from_flow(
-            flow=self.main_flow,
-            name=self._main_flow_deploy_name,
-            storage=self.storage,
-            work_queue_name=f"{self.main_flow.name}-queue",
+        self.flows["main"].from_source(
+            source=source,
             entrypoint=f"{calling_package.replace('.', '/')}/{calling_file}:{datajoint_flow_name}_main_flow",
+        ).deploy(
+            name=self._main_flow_deploy_name,
+            work_queue_name=work_queue_name or f"{self.main_flow.name}-queue",
+            work_pool_name=work_pool_name or f"{self.main_flow.name}-work-pool",
+            image=image,
+            build=False,
+            push=False
         )
-        self.deployments["main"] = main_deployment
 
-        trigger_deployment = Deployment.build_from_flow(
-            flow=self.trigger_flow,
-            name=self._trigger_flow_deploy_name,
-            work_queue_name=f"{self.trigger_flow.name}-queue",
-            storage=self.storage,
-            schedule=IntervalSchedule(interval=60 * 60 * 24),
+        self.flows["trigger"].from_source(
+            source=source,
             entrypoint=f"{calling_package.replace('.', '/')}/{calling_file}:{datajoint_flow_name}_trigger_flow",
+        ).deploy(
+            name=self._trigger_flow_deploy_name,
+            work_queue_name=work_queue_name or f"{self.main_flow.name}-queue",
+            work_pool_name=work_pool_name or f"{self.main_flow.name}-work-pool",
+            cron="*/10 * * * *",
+            image=image,
+            build=False,
+            push=False
         )
-        self.deployments["trigger"] = trigger_deployment
-
-        self.deployments["main"].apply()
-        self.deployments["trigger"].apply()
